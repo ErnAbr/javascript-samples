611afe3eb1ef4068c1d939068727610078bf4afa
ErnAbr <ern_abr@yahoo.com>
2023-08-16 21:10:08 +0300

    task8Completed

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,34 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator(firstNumber, secondNumber) {
+  this.firstNumber = firstNumber;
+  this.secondNumber = secondNumber;
+
+  this.sum = function () {
+    const result = this.firstNumber + this.secondNumber;
+    return result;
+  };
+
+  this.subtraction = function () {
+    const result = this.firstNumber - this.secondNumber;
+    return result;
+  };
+
+  this.multiplication = function () {
+    const result = this.firstNumber * this.secondNumber;
+    return result;
+  };
+
+  this.division = function () {
+    const result = this.firstNumber / this.secondNumber;
+    return result;
+  };
+}
+
+const calculate = new Calculator(5, 10);
+console.log(calculate.sum());
+console.log(calculate.subtraction());
+console.log(calculate.multiplication());
+console.log(calculate.division());
