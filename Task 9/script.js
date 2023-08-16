2aee3d4f268ef84e79d610814ea8576506edbd2a
ErnAbr <ern_abr@yahoo.com>
2023-08-16 21:22:49 +0300

    task9Completed

--- Task 9/script.js
+++ Task 9/script.js
@@ -6,3 +6,22 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kitu atveju false 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+
+  wasExpensive = () => {
+    const budgetValue = this.budget > 100000000 ? true : false;
+    return budgetValue;
+  };
+}
+
+const newMovie = new Movie("Terminator", "Schwarzenegger", 6400000);
+console.log(newMovie.title);
+console.log(newMovie.director);
+console.log(newMovie.budget);
+console.log(newMovie.wasExpensive());
