97a66836a334f2cfe93897cb9608fe1afb56a6ee
ErnAbr <ern_abr@yahoo.com>
2023-08-16 20:14:15 +0300

    task4Completed

--- Task 4/script.js
+++ Task 4/script.js
@@ -8,3 +8,34 @@ nurodomas gamintojas ir jo pagaminti modeliai.
 Pastaba: Informacija apie automobilį (brand) (jo kortelė) bei turi turėti 
 bent minimalų stilių;
 -------------------------------------------------------------------------- */
+const output = document.querySelector("#output");
+
+const carData = async () => {
+  const response = await fetch("./cars.json");
+  const cars = await response.json();
+  return cars;
+};
+
+const printCars = async () => {
+  const cars = await carData();
+
+  cars.forEach((car) => {
+    const carCard = document.createElement("div");
+    carCard.className = "card-style";
+    const carBrand = document.createElement("h2");
+    const cardList = document.createElement("ul");
+
+    carBrand.textContent = car.brand;
+    carCard.append(carBrand, cardList);
+    output.append(carCard);
+
+    car.models.forEach((model) => {
+      console.log(model);
+      const carListItem = document.createElement("li");
+      carListItem.textContent = model;
+      cardList.append(carListItem);
+    });
+  });
+};
+
+printCars();
