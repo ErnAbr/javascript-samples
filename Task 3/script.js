de1101cd3b38a9c5aea0f8eef122f6f45e0635e0
ErnAbr <ern_abr@yahoo.com>
2023-08-16 19:32:10 +0300

    task3Completed

--- Task 3/script.js
+++ Task 3/script.js
@@ -10,4 +10,31 @@ Paspaudus mygtuką "Show users":
 Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+const showUsersButton = document.querySelector("#btn");
+const output = document.querySelector("#output");
+const message = document.querySelector("#message");
+
+const getUsers = async () => {
+  const response = await fetch(ENDPOINT);
+  const users = await response.json();
+  return users;
+};
+
+showUsersButton.addEventListener("click", () => {
+  getUsers().then((users) => {
+    message.textContent = "";
+    users.forEach((user) => {
+      const card = document.createElement("div");
+      const image = document.createElement("img");
+      const login = document.createElement("p");
+      card.className = "card-style";
+      output.className = "output-style";
+      image.src = user.avatar_url;
+      login.textContent = user.login;
+
+      card.append(image, login);
+      output.append(card);
+    });
+  });
+});
